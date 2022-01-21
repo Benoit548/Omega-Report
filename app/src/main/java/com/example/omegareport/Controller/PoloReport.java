96240a6a8a2cff4f8c2261766b7842a7ce003aa3
Benoit548 <benoit.lusset@hotmail.fr>
2022-01-22 00:58:20 +0100

    Polo droit fini

---
+++ app/src/main/java/com/example/omegareport/Controller/PoloReport.java
@@ -0,0 +1,57 @@
+package com.example.omegareport.Controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.omegareport.R;
+
+public class PoloReport extends AppCompatActivity {
+
+    private TextView mTeamName;
+    private TextView mTime;
+    private String teamName = "Omega Squad";
+    private String time = "00:00";
+    private ImageView mBombay;
+    private ImageView mParis;
+    private ImageView mSaoPaulo;
+    private ImageView mPekin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_polo_report);
+
+        mTeamName = findViewById(R.id.report_textView_team_name);
+        mTime = findViewById(R.id.report_textView_timer);
+
+        mBombay = findViewById(R.id.poloReport_imageView_bombay);
+        mParis = findViewById(R.id.poloReport_imageView_paris);
+        mSaoPaulo = findViewById(R.id.poloReport_imageView_saoPaulo);
+        mPekin = findViewById(R.id.poloReport_imageView_pekin);
+
+        final Intent myIntent = getIntent();
+        if(null!=myIntent) {
+            final Bundle extras2 = myIntent.getExtras();
+            if (extras2 != null) {
+                teamName = extras2.getString(SyncStateContract.Constants.ACCOUNT_NAME);
+                time = extras2.getString(SyncStateContract.Constants.DATA);
+            }
+        }
+        mTeamName.setText("Omega Squad");
+        if(!(teamName.equals(""))){ mTeamName.setText(teamName);}
+        mTime.setText(time);
+
+        if(!(getIntent().getExtras().getBoolean("bombay"))){mBombay.setImageResource(R.drawable.nuc_false);}
+        if(!(getIntent().getExtras().getBoolean("paris"))){mParis.setImageResource(R.drawable.nuc_false);}
+        if(!(getIntent().getExtras().getBoolean("saoPaulo"))){mSaoPaulo.setImageResource(R.drawable.nuc_false);}
+        if(!(getIntent().getExtras().getBoolean("pekin"))){mPekin.setImageResource(R.drawable.nuc_false);}
+
+    }
+}
--- app/src/main/java/com/example/omegareport/Controller/PoloToFill.java
+++ app/src/main/java/com/example/omegareport/Controller/PoloToFill.java
@@ -2,15 +2,97 @@ package com.example.omegareport.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.omegareport.Model.TeamName;
 import com.example.omegareport.R;
 
+import java.util.Locale;
+
 public class PoloToFill extends AppCompatActivity {
+
+    private Button mSubmitButton;
+    private TextView mCongratsText;
+    private EditText mName;
+    private EditText mTime;
+    private TeamName mTeamName;
+    private CheckBox mCheckEnglish;
+    private CheckBox mBombay;
+    private CheckBox mParis;
+    private CheckBox mSaoPaulo;
+    private CheckBox mPekin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_polo_to_fill);
+
+        mCongratsText =findViewById(R.id.polo_TextView_congrats);
+        mSubmitButton =findViewById(R.id.polo_button_submit);
+        mName = findViewById(R.id.polo_editText_TeamName);
+        mTime = findViewById(R.id.polo_EditText_time);
+        mCheckEnglish = findViewById(R.id.polo_checkBox_english);
+
+        mBombay = findViewById(R.id.poloFill_checkBox_Bombay);
+        mParis = findViewById(R.id.poloFill_checkBox_Paris);
+        mSaoPaulo = findViewById(R.id.poloFill_checkBox_SaoPaulo);
+        mPekin = findViewById(R.id.poloFill_checkBox_Pekin);
+
+        mBombay.setChecked(true);
+        mParis.setChecked(true);
+        mSaoPaulo.setChecked(true);
+        mPekin.setChecked(true);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if(TextUtils.isEmpty(mTime.getText())){
+                    Toast.makeText(PoloToFill.this,R.string.error_no_time,Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(mCheckEnglish.isChecked()){
+                    String languageToLoad  = "en"; // your language
+                    Locale locale = new Locale(languageToLoad);
+                    Locale.setDefault(locale);
+                    Configuration config = new Configuration();
+                    config.locale = locale;
+                    getBaseContext().getResources().updateConfiguration(config,
+                            getBaseContext().getResources().getDisplayMetrics());
+                }
+
+                else{
+                    String languageToLoad  = "fr"; // your language
+                    Locale locale = new Locale(languageToLoad);
+                    Locale.setDefault(locale);
+                    Configuration config = new Configuration();
+                    config.locale = locale;
+                    getBaseContext().getResources().updateConfiguration(config,
+                            getBaseContext().getResources().getDisplayMetrics());
+                }
+
+                Intent myIntent = new Intent(PoloToFill.this, PoloReport.class);
+                final Bundle extras = new Bundle();
+                extras.putString(SyncStateContract.Constants.ACCOUNT_NAME,mName.getText().toString());
+                extras.putString(SyncStateContract.Constants.DATA,mTime.getText().toString());
+                myIntent.putExtras(extras);
+                myIntent.putExtra("bombay", mBombay.isChecked());
+                myIntent.putExtra("paris", mParis.isChecked());
+                myIntent.putExtra("saoPaulo", mSaoPaulo.isChecked());
+                myIntent.putExtra("pekin", mPekin.isChecked());
+                startActivity(myIntent);
+            }
+        });
     }
 }
